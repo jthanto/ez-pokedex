167c72b6ae74896d85c5e4d6afe626af4a0ef777
jthanto <jthanto@fixedyour.net>
2016-06-23 20:59:46 +0200

    Refactoring the dex to have all mons in a json-file, using mustache to preview it. The mustache might be used for more intricate stuff later. Also now when changing the state of a row (caught/un-caught) it will remove based on active filter. Also tablesorter library was added, but is not used yet, as it didn't make sense until i apply global search filtering. I think tablesorter has filtering functionality. Also added import functionality. Now it uses JSON, but it MIGHT get changed, #crypto ;)

---
+++ pokedex.js
@@ -0,0 +1,133 @@
+var _pokemon = {};
+
+var rowClick = function(){
+    if($(this).hasClass('got-it')){
+        $(this).removeClass('got-it');
+        removeFromLocalStorage($(this).find('.id').html());
+    } else {
+        $(this).addClass('got-it');
+        saveToLocalStorage($(this).find('.id').html());
+    }
+    if(shouldFilterHide($(this).hasClass('got-it'))){
+        $(this).fadeOut(400);
+    }
+    loadStatus();
+};
+
+var shouldFilterHide = function(gotIt){
+    switch($('select#filter option:selected').val()){
+        case 'caught':
+            return !gotIt;
+        case 'un-caught':
+            return gotIt;
+        default:
+            return false;
+    }
+};
+
+var saveToLocalStorage = function(id){
+    var pokedex = {};
+    if(localStorage.pokedex){
+        pokedex = JSON.parse(localStorage.pokedex);
+    }
+    pokedex[id] = true;
+    localStorage.pokedex = JSON.stringify(pokedex);
+};
+
+var removeFromLocalStorage = function(id){
+    var pokedex = {};
+    if(localStorage.pokedex){
+        pokedex = JSON.parse(localStorage.pokedex);
+    }
+    delete pokedex[id];
+    localStorage.pokedex = JSON.stringify(pokedex);
+};
+
+var setupCollected = function(){
+    var pokedex = {};
+    if(localStorage.pokedex){
+        pokedex = JSON.parse(localStorage.pokedex);
+    }
+    $.each(pokedex, function(idx){
+        $('td.id:contains('+idx+')').parents('tr').addClass('got-it');
+    });
+};
+
+var exportJSON = function(){
+    if(localStorage.pokedex){
+        $('textarea#user_data').html(localStorage.pokedex).show();
+    } else {
+        alert('No save data');
+    }
+};
+
+var importJSON = function(){
+    var $data = $('textarea#user_data').val();
+    console.log($data);
+    localStorage.pokedex = $('textarea#user_data').val();
+};
+
+var applyFilter = function(){
+    var $table = $('table#dex_table');
+    var filter = $(this).find('option:selected').val();
+    switch(filter){
+        case 'caught':
+            $table.find('tbody tr').hide();
+            $table.find('tbody tr.got-it').show();
+            break;
+        case 'un-caught':
+            $table.find('tbody tr').hide();
+            $table.find('tbody tr:not(.got-it)').show();
+            break;
+        default:
+            showAll();
+    }
+};
+
+var showAll = function(){
+    $('table#dex_table').find('tbody tr').show();
+};
+
+var loadStatus = function(){
+    var tpl = $('#status_template').html();
+    var $dexTable = $('#dex_table');
+    var total = $dexTable.find('tbody tr').length;
+    var caught = $dexTable.find('tbody tr.got-it').length;
+    var diff = total - caught;
+    $('#status').html(Mustache.render(tpl, {total: total, caught: caught, missing: diff}));
+};
+
+var loadPokemon = function(callback){
+    $.ajax({
+        url: "pokedex.json",
+        dataType: 'json',
+        success: function(pokemon){
+            _pokemon = pokemon;
+            var tpl = $('#pokedex_row_template').html();
+            var html = Mustache.render(tpl, {pokemon: _pokemon});
+            $('#dex_table').find('tbody').append(html);
+        }
+    }).done(function(){
+        if($.isFunction(callback)){
+            callback();
+        }
+    });
+};
+
+var initialize = function(){
+    loadPokemon(function(){
+        setupCollected();
+        loadStatus();
+        $('table#dex_table').find('tbody tr').click(rowClick);
+        $('button#export_json').click(exportJSON);
+        $('button#import_json').click(importJSON);
+        $('#options').click(function(){$('#options_container').toggle()});
+        $('select#filter').change(applyFilter);
+
+    });
+};
+
+$(document).ready(function(){
+    initialize();
+});
+
