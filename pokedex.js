c4cbd20b52a790d157bab13c0d1a3103d27d445f
Jan Thomas Hanto <jthanto@fixedyour.net>
2016-06-24 00:15:55 +0200

    Added filtering, and sticky headers with tablesorter. Still need some work.

--- pokedex.js
+++ pokedex.js
@@ -14,6 +14,45 @@ var rowClick = function(){
     loadStatus();
 };
 
+var sorterConfig = {
+    widgets: ['stickyHeaders', 'filter' ],
+    selectorHeaders: 'thead th, thead td',
+    widgetOptions: {
+
+        // extra class name added to the sticky header row
+        stickyHeaders : 'sticky',
+        // number or jquery selector targeting the position:fixed element
+        stickyHeaders_offset : 0,
+        // added to table ID, if it exists
+        stickyHeaders_cloneId : '-sticky',
+        // trigger "resize" event on headers
+        stickyHeaders_addResizeEvent : true,
+        // if false and a caption exist, it won't be included in the sticky header
+        stickyHeaders_includeCaption : true,
+        // The zIndex of the stickyHeaders, allows the user to adjust this to their needs
+        stickyHeaders_zIndex : 2,
+        // jQuery selector or object to attach sticky header to
+        stickyHeaders_attachTo : null,
+        // jQuery selector or object to monitor horizontal scroll position (defaults: xScroll > attachTo > window)
+        stickyHeaders_xScroll : null,
+        // jQuery selector or object to monitor vertical scroll position (defaults: yScroll > attachTo > window)
+        stickyHeaders_yScroll : null,
+
+        // scroll table top into view after filtering
+        stickyHeaders_filteredToTop: true
+
+
+
+        // *** REMOVED jQuery UI theme due to adding an accordion on this demo page ***
+        // adding zebra striping, using content and default styles - the ui css removes the background from default
+        // even and odd class names included for this demo to allow switching themes
+        // , zebra   : ["ui-widget-content even", "ui-state-default odd"]
+        // use uitheme widget to apply defauly jquery ui (jui) class names
+        // see the uitheme demo for more details on how to change the class names
+        // , uitheme : 'jui'
+    }
+}
+
 var shouldFilterHide = function(gotIt){
     switch($('select#filter option:selected').val()){
         case 'caught':
@@ -118,7 +157,9 @@ var initialize = function(){
     loadPokemon(function(){
         setupCollected();
         loadStatus();
-        $('table#dex_table').find('tbody tr').click(rowClick);
+        var $dexTable = $('table#dex_table');
+        $dexTable.tablesorter(sorterConfig);
+        $dexTable.find('tbody tr').click(rowClick);
         $('button#export_json').click(exportJSON);
         $('button#import_json').click(importJSON);
         $('#options').click(function(){$('#options_container').toggle()});
